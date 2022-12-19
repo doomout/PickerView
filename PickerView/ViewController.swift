a2d30ed8c6ab1413de2a4b8137977dc03f2b7616
doomout <winquake3@hanmail.net>
2022-12-19 19:25:57 +0900

    피커에서 선택시 하단에 이미지 출력까지 구현

--- PickerView/ViewController.swift
+++ PickerView/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     let MAX_ARRY_NUM = 10
     let PICKER_VIEW_COLUMN = 1
+    var imageArray = [UIImage?]()
     var imageFileName = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg" ]
     
     @IBOutlet var pickerImage: UIPickerView!
@@ -20,6 +21,19 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //i라는 변수를 0~MAX_ARRY_NUM 까지 반복한다.
+        for i in 0 ..< MAX_ARRY_NUM {
+            //생성된 이미지를 image에 할당한다.
+            let image = UIImage(named: imageFileName[i])
+            //배열에 추가한다.
+            imageArray.append(image)
+        }
+        //첫번째 파일명을 출력한다.
+        lblImageFileName.text = imageFileName[0]
+        
+        //첫번째 이미지를 나타낸다
+        imageView.image = imageArray[0]
     }
     //피커 뷰에게 컴포넌트의 수를 정수 값으로 넘겨주는 델리게이트 메서드
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
@@ -40,6 +54,9 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     //가져온 값은 lblImageFileName 에 저장한다.
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         lblImageFileName.text = imageFileName[row]
+        imageView.image = imageArray[row]
     }
+    
+  
 }
 
